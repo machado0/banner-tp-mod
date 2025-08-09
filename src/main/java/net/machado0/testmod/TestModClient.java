0d65f57b4c9c5e13e1f90a63b064b827a1b06a92
machado0 <medmachado27@gmail.com>
2025-08-09 09:39:45 -0300

    feat: initial commit

---
+++ src/main/java/net/machado0/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.machado0.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
